7f9f6cd0dda8da69369ade1c184b55e95500a8b7
juanda0093 <juan_da1993@outlook.es>
2017-05-21 22:51:51 -0500

    promer Cambio
    hola

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script src="Recursos/JS/gestionSignOff.js" type="text/javascript"></script>
+        <script src="Recursos/JS/gestionLogIn.js" type="text/javascript"></script>
+        <script src="Recursos/JS/gestionProspecto.js" type="text/javascript"></script>
+        <link href="Recursos/CSS/estilos.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <?php
+        session_start();
+        if (isset($_SESSION['nickname'])) {
+            include 'Vista/masterPage.php';
+        } else {
+            include 'Vista/logIn.php';
+        }
+        ?>
+    </body>
+</html>
